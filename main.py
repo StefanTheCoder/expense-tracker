d7e8de8ab897520551c10a3bd2fd634ddfebf3bf
StefanTheCoder <stefan.ekn93@gmail.com>
2026-07-19 00:38:11 +0200

    Use environment variable for Firebase credentials

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_creds = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
